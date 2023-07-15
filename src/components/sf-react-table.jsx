b3de1d8d1a6593929b529bbd558e4850f2f115c3
stephenPolFajardo <stepdfuture@gmail.com>
2023-07-15 22:25:10 +0800

    initial development

--- src/components/sf-react-table.jsx
+++ src/components/sf-react-table.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './tableStyle.css';
 
 const ReactTable = ({ data, columns }) => {
     return (
-        <table style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <table>
         <thead>
-          <tr style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <tr>
             {columns.map((column) => (
-              <th style={{ padding: 8 }} key={column}>{column}</th>
+              <th key={column}>{column}</th>
             ))}
           </tr>
         </thead>
         <tbody>
           {data.map((row, index) => (
-            <tr key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <tr key={index}>
               {columns.map((column) => (
-                <td style={{ padding: 8 }} key={column}>{row[column]}</td>
+                <td key={column}>{row[column]}</td>
               ))}
             </tr>
           ))}
---
+++ src/styles/tableStyles.jsx
@@ -0,0 +1,12 @@
+export const styles = { 
+    distplayFlex: {
+        display: 'flex',
+    },
+
+    flexDirectionColumn: {
+        flexDirection: 'column',
+    },
+    alignItemsCenter: {
+        alignItems: 'center',
+    }
+}
